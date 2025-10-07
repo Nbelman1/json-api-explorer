defecc50e301529900a16ec6a998c99e5ba8e26f
Noah <nbelman1@gmail.com>
2025-10-07 08:48:35 -0400

    Added fetch function to display posts

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// so much empty
+const postList = document.getElementById("postList");
+const fetchButton = document.getElementById("fetchButton");
+const submitButton = document.getElementById("submit-button");
+const titleInput = document.getElementById("titleInput");
+const bodyInput = document.getElementById("bodyInput");
+
+// GET request to retrieve posts 
+fetchButton.addEventListener("click", function() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(function(response) {
+        return response.json(); // converts response to JSON
+    })
+    .then(function(data) {
+        console.log(data);
+        return data;
+    })
+    .then(function(data) { // loop through data, inject text into body of HTML
+        for (let post of data) {
+            postList.innerHTML += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p> 
+            `;
+        }
+    })
+});
+
+// POST method
+submitButton.addEventListener("click", function() {
+    // grab user inputs
+    let postObj = `{
+        "title": ${titleInput.value},
+        "body": ${bodyInput.value}
+    }`;
+    let postBody = JSON.stringify(postObj);
+    
+    // send user inputs to API
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST", 
+        body: postBody
+    })
+
+    console.log("Sending your post ...");
+
+});
